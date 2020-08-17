abc3e3d8f745f10447d7f4784a0cd63a2d286c8d
Steven van den Broek <stevenvdb@live.nl>
2020-08-17 12:00:25 +0200

    Make all example notebooks except static-canvcompile

--- shell.nix
+++ shell.nix
@@ -11,7 +11,7 @@ let
 
   iHaskell = jupyter.kernels.iHaskellWith {
     name = "haskell";
-    packages = p: with p; [ diagrams ihaskell-diagrams ];
+    packages = p: with p; [ diagrams ihaskell-diagrams ihaskell-blaze ihaskell-charts SVGFonts palette blaze Chart MissingH ];
   };
 
   jupyterEnvironment =
